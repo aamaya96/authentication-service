9607113b5ac74d74c62087dcfa088cc5fe3dfb9b
Arnold Amaya <arnold.amaya@outlook.com>
2019-04-07 08:47:55 -0600

    Added basic user model schema

---
+++ src/db/models/user-model.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        unique: true, 
+        validate(value) {
+            if(!validator.isEmail(value)){
+                throw new Error('Email has an invalid format');
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+        trim: true
+    }
+});
+
+userSchema.pre('save', async function(next) {
+    const user = this;
+    
+    if(user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8);
+    }
+    next();
+});
+
+
+const User = mongoose.model('Users', userSchema);
+
+module.exports = User;
